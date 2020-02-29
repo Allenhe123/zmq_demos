d88fef3ed3c506e9b5436c09ff4ca881689d0b9b
Allenhe123 <931317255@qq.com>
2020-03-01 01:42:20 +0800

    zmq+protobuf

--- src/cppzmq_demo_srv.cc
+++ src/cppzmq_demo_srv.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "video.pb.h"
 
 int main () {
     zmq::context_t context (1);
@@ -12,14 +13,18 @@ int main () {
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
     while (true) {
-        zmq::message_t request;
-        socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
+        zmq::message_t msg;
+        socket.recv (&msg);
+        zmq::demo::Frame frame; 
+        frame.ParseFromArray(msg.data(), msg.size());
+        std::cout << "srv recv Frame: " << frame.frame_id() << " " << frame.width() << " " << frame.height() << " " <<
+                    frame.step() << " " << frame.address() << " " << frame.address_yuv420p() << std::endl;
 
         //  Send reply back to client
-        zmq::message_t reply(5);
-        memcpy((void *) reply.data(), "World", 5);
-        socket.send(reply);
+        zmq::message_t reply(32);
+        memset(reply.data(), 0, 32);
+        snprintf((char*)reply.data(), 32, "srv recv frame: %d\n", frame.frame_id());
+        socket.send(reply, ZMQ_DONTWAIT);
     }
     return 0;
 }
